464cd0bb90e174491cd02ede81bbae3ba1391c16
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2025-06-11 14:23:14 +0300

    Protecting Routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,25 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+// const isProtectedRoute = createRouteMatcher(['/user-profile']);
+const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
+
+// export default clerkMiddleware(async (auth, req) => {
+// 	if (!isPublicRoute(req)) await auth.protect();
+// })
+
+export default clerkMiddleware(async (auth, req) => {
+	const { userId, redirectToSignIn } = await auth()
+	if(!userId && !isPublicRoute(req)) {
+		//add custom logic to run before redirecting
+		
+		return redirectToSignIn()
+	}
+})
+
+// export default clerkMiddleware(async (auth, req) => {
+// 	if (isProtectedRoute(req)) await auth.protect();
+// })
+
 
 export const config = {
   matcher: [
@@ -10,3 +29,4 @@ export const config = {
     '/(api|trpc)(.*)',
   ],
 };
+
